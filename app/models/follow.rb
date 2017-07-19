8edda63cdde8d94af68988d188de8c2a8a148f3d
Joel Guerra <joelaguerra1@gmail.com>
2017-07-19 11:01:55 -0700

    adds seeds

--- app/models/follow.rb
+++ app/models/follow.rb
@@ -1,3 +1,4 @@
 class Follow < ApplicationRecord
   belongs_to :user
+  validates_uniqueness_of :followee_id, :scope => :foller_id
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+  User.create(username: Faker::Internet.user_name, name: Faker::Name.name, password: 'pw')
+end
+
+30.times do
+  Follow.create(followee_id: rand(1..10), follower_id: rand(1..10))
+end
