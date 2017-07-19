74cded9aee2fd3d24ef68402ad5b4509067a372f
Joel Guerra <joelaguerra1@gmail.com>
2017-07-19 10:41:13 -0700

    writes migrations

--- db/migrate/20170719173324_create_entries.rb
+++ db/migrate/20170719173324_create_entries.rb
@@ -1,7 +1,9 @@
 class CreateEntries < ActiveRecord::Migration[5.0]
   def change
     create_table :entries do |t|
-
+      t.string :title, null: false
+      t.string :url, null: false
+      t.string :caption
       t.timestamps
     end
   end
--- db/migrate/20170719173327_create_follows.rb
+++ db/migrate/20170719173327_create_follows.rb
@@ -1,7 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[5.0]
   def change
     create_table :follows do |t|
-
+      t.integer :followee_id, null: false
+      t.integer :follower_id, null: false
       t.timestamps
     end
   end
