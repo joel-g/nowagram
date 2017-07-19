255709067127d1c97049c22e9ac963f2386bdba9
Joel Guerra <joelaguerra1@gmail.com>
2017-07-19 10:54:08 -0700

    writes models and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  has_many :entries
+  has_many :followers, class_name: 'User', foreign_key: follower_id
+  has_many :followees, class_name: 'User', foreign_key: followee_id
 end
